03f84eff2d56eeac21435cdee559dd08924537c8
G Kanishk Samurai <156745281+kanishksamuraig@users.noreply.github.com>
2026-04-08 07:18:39 +0530

    Add: island-perimeter - submission-1

---
+++ Data Structures & Algorithms/island-perimeter/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        def neighbours(node):
+            r,c=node
+            neighbourlist=[]
+            for dr,dc in [(0,-1),(-1,0),(0,1),(1,0)]:
+                nr=dr+r
+                nc=dc+c
+                if 0<= nr < len(grid) and 0 <= nc <len(grid[0]) and grid[nr][nc]==1:
+                    neighbourlist.append((nr,nc))
+            return neighbourlist
+        d={0:4, 1:3, 2:2, 3:1, 4:0}
+        hash=set()
+        start=(-1,-1)
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==1:
+                    start=(i,j)
+        queue=deque()
+        queue.append(start)
+        perimeter=0
+        while queue:
+            node=queue.popleft()
+            hash.add(node)
+            neighbour1=neighbours(node)
+            perimeter+=d[len(neighbour1)]
+            for neighbour in neighbour1:
+                if neighbour not in hash:
+                    hash.add(neighbour)
+                    queue.append(neighbour)
+        return perimeter
