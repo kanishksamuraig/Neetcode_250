458961c2af05caf5539e187d05e28d57d500adc6
G Kanishk Samurai <156745281+kanishksamuraig@users.noreply.github.com>
2026-07-02 06:33:22 +0530

    Add: generate-parentheses - submission-5

---
+++ Data Structures & Algorithms/generate-parentheses/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        lst=[]
+        def genpar(curr,open,close,n):
+            if open+close==2*n:
+                lst.append(curr)
+            
+            if open<n:
+                genpar(curr+"(",open+1,close,n)
+            
+            if close<open:
+                genpar(curr+")",open,close+1,n)
+        
+        genpar("",0,0,n)
+        return lst
